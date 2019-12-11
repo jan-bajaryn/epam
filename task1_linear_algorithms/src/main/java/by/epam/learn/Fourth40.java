908fe0554553834fa5115b8e1fac8c6159602937
Jan <jabajaryn@gmail.com>
2019-12-11 03:15:18 +0300

    initial commit

---
+++ task1_linear_algorithms/src/main/java/by/epam/learn/Fourth40.java
@@ -0,0 +1,21 @@
+package by.epam.learn;
+
+public class Fourth40 {
+
+    //    -2х + 3х2 - 4х3
+    private double first(double x) {
+        return x * (-2 + x * (3 - 4 * x));
+    }
+
+    //    -2х + 3х2 - 4х3
+    private double second(double x) {
+        return 1 + x * (2 + x * (3 + 4 * x));
+    }
+
+    public static void main(String[] args) {
+        Fourth40 fourth40 = new Fourth40();
+        System.out.println(fourth40.first(25));
+        System.out.println(fourth40.second(25));
+
+    }
+}
