ee10023f03e655dec50352b856f7cc94f56bc181
Jan <jabajaryn@gmail.com>
2019-12-15 20:42:08 +0300

    Sum to text added

--- task1_2_3/src/main/java/by/epam/learn/task3/sumtotext/Runner.java
+++ task1_2_3/src/main/java/by/epam/learn/task3/sumtotext/Runner.java
@@ -1,7 +1,11 @@
 package by.epam.learn.task3.sumtotext;
 
+import by.epam.learn.task3.sumtotext.parser.SumToTextParser;
+
 public class Runner {
     public static void main(String[] args) {
-
+        SumToTextParser sumToTextParser = new SumToTextParser();
+        System.out.println(sumToTextParser.parseText(0));
+//        System.out.println(Character.getNumericValue('0'));
     }
 }
