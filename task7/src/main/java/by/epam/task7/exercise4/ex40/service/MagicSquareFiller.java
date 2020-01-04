5601c633075ba62b4aea6b492900f90d16f6acfe
Jan <jabajaryn@gmail.com>
2020-01-04 16:06:55 +0300

    code cleaned

--- task7/src/main/java/by/epam/task7/exercise4/ex40/service/MagicSquareFiller.java
+++ task7/src/main/java/by/epam/task7/exercise4/ex40/service/MagicSquareFiller.java
@@ -62,12 +62,10 @@ public class MagicSquareFiller {
         }
 
 
-//        swapAutomatically(squareMatrix, -1);
     }
 
     private void fillRemain(List<Integer> list, SquareMatrix squareMatrix, int symbol) {
         int counter = 0;
-        System.out.println(list);
         for (int i = squareMatrix.calcRows() - 1; i >= 0; i--) {
             for (int j = squareMatrix.calcRows() - 1; j >= 0; j--) {
                 if (squareMatrix.calcElem(i, j) == symbol) {
@@ -76,82 +74,6 @@ public class MagicSquareFiller {
             }
         }
     }
-
-//    private void swapAutomatically(SquareMatrix squareMatrix, int symbol) {
-//        int beginCounter = 1;
-//        int lastCounter = squareMatrix.calcRows();
-//
-//        int bRow = 0;
-//        int bCol = 0;
-//
-//        Pair bPair = new Pair(bRow,bCol);
-//
-//        int lRow = lastCounter - 1;
-//        int lCol = lRow;
-//
-//        Pair lPair = new Pair(lRow,lCol);
-
-//        int fPos =0;
-//        int lPos = lRow;
-
-//        boolean isDone = false;
-//        while (bRow <= lRow /*|| bCol < lCol*/) {
-//            for (int i = bRow; i <= lRow; i++) {
-//                for (int j = bCol; j < squareMatrix.calcRows(); j++) {
-//                    if (squareMatrix.calcElem(bRow, bCol) == symbol) {
-//                        break;
-//                    }
-//                }
-//                bCol = 0;
-//            }
-//            Pair fstPair = findFirstPositions(squareMatrix,bRow,lRow,bCol)
-
-    // find next position with symbol
-
-//            bPair = findFirstPosition(squareMatrix, bPair);
-//            if (bPair != null) {
-//                lPair = findSecondPosition(squareMatrix, lPair);
-//
-//                squareMatrix.setElement(bPair.getFst(),bPair.getSec(),lastCounter);
-//                squareMatrix.setElement(lPair.getFst(),lPair.getSec(),beginCounter);
-//            }
-
-    // find next position with symbol
-
-
-//        }
-//
-//    }
-
-//    private Pair straightNextSquareMatrix(int rows, Pair pair) {
-//        if (pair.getFst() == rows - 1) {
-//            if (pair.getSec() == rows - 1) {
-//                return new Pair(pair.getFst(), pair.getSec());
-//            } else {
-//                return new Pair(0, pair.getSec() + 1);
-//            }
-//        } else {
-//            return new Pair(pair.getFst() + 1, pair.getSec());
-//        }
-//    }
-//
-//    private Pair straightPrevSquareMatrix(int rows, Pair pair) {
-//        if (pair.getFst() == 0) {
-//            if (pair.getSec() == 0) {
-//                return new Pair(0, 0);
-//            } else {
-//                return new Pair(rows - 1, pair.getSec() - 1);
-//            }
-//        } else {
-//            return new Pair(pair.getFst() - 1, pair.getSec());
-//        }
-//    }
-
-//    private void swap(SquareMatrix squareMatrix, int bRow, int bCol, int lRow, int lCol) {
-//        int temp = squareMatrix.calcElem(bRow, bCol);
-//        squareMatrix.setElement(bRow, bCol, squareMatrix.calcElem(lRow, lCol));
-//        squareMatrix.setElement(lRow, lCol, temp);
-//    }
 
     private List<Integer> fillSymbolStraight(SquareMatrix squareMatrix, int symbol) {
         List<Integer> list = new ArrayList<>();
@@ -183,7 +105,6 @@ public class MagicSquareFiller {
 
 
         int begin = rows / 2 + 1;
-        System.out.println("begin = " + begin);
 
 
         int count = rows * rows;
@@ -193,7 +114,6 @@ public class MagicSquareFiller {
 
         int i = 1;
         do {
-            System.out.println("Current = " + i);
             squareMatrix.setElement(curRow, curCol, i);
             Pair next = next(rows, curRow, curCol, squareMatrix);
             curRow = next.getFst();
@@ -205,11 +125,9 @@ public class MagicSquareFiller {
 
     private Pair next(int rows, int curRow, int curColumn, SquareMatrix matrix) {
 
-        System.out.println("curRow = " + curRow + ", curColumn = " + curColumn);
         int row = (curRow - 1) < 0 ? rows - 1 : (curRow - 1);
         int col = (curColumn + 1) % rows;
 
-        System.out.println("row = " + row + ", column = " + col);
         if (matrix.calcElem(row, col) == -1) {
             return new Pair(row, col);
         }
