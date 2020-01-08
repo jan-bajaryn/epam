ed6611d6ef673321b534c4474f6fdf9e21382d6d
Jan <jabajaryn@gmail.com>
2020-01-08 03:14:38 +0300

    some

--- task9/src/main/java/by/epam/task9/example4/account/controller/Runner.java
+++ task9/src/main/java/by/epam/task9/example4/account/controller/Runner.java
@@ -1,7 +1,52 @@
 package by.epam.task9.example4.account.controller;
 
+import by.epam.task9.example4.account.dao.AccountRepo;
+import by.epam.task9.example4.account.dao.UserRepo;
+import by.epam.task9.example4.account.dao.reader.TextFileReader;
+import by.epam.task9.example4.account.entity.Account;
+import by.epam.task9.example4.account.entity.User;
+import by.epam.task9.example4.account.parser.AccountParser;
+import by.epam.task9.example4.account.parser.WrongDataException;
+import by.epam.task9.example4.account.view.WriteAccountListToFile;
+
+import java.io.IOException;
+
 public class Runner {
     public static void main(String[] args) {
+        AccountParser accountParser = new AccountParser();
+        TextFileReader textFileReader = new TextFileReader();
+        WriteAccountListToFile writeAccountListToFile = new WriteAccountListToFile();
+        UserRepo userRepo = UserRepo.getInstance();
+        AccountRepo accountRepo = AccountRepo.getInstance();
+        User user = new User("Jan", "Kozlov");
+        Account account = new Account(user.getId(), 24, true);
+        userRepo.save(user);
+        accountRepo.save(account);
 
+        user = new User("Pawel", "Kuznecov");
+        account = new Account(user.getId(), 2, true);
+        userRepo.save(user);
+        accountRepo.save(account);
+
+        user = new User("III", "CCC");
+        account = new Account(user.getId(), 345, true);
+        userRepo.save(user);
+        accountRepo.save(account);
+
+
+//        try {
+//            writeAccountListToFile.write("iha.txt", accountRepo.findActive());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            String[] s = textFileReader.readLinesFromFile("my.txt");
+            System.out.println(accountParser.stringToAccountList(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WrongDataException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ task9/src/main/java/by/epam/task9/example4/account/parser/WrongDataException.java
@@ -0,0 +1,18 @@
+package by.epam.task9.example4.account.parser;
+
+public class WrongDataException extends Exception {
+    public WrongDataException() {
+    }
+
+    public WrongDataException(String message) {
+        super(message);
+    }
+
+    public WrongDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WrongDataException(Throwable cause) {
+        super(cause);
+    }
+}
