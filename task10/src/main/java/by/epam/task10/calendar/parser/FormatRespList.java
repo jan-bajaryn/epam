682bb9dcc81dbdc83ae7080121cac8c79e2b9870
Jan <jabajaryn@gmail.com>
2020-02-01 02:41:57 +0300

    changes in task10

--- task10/src/main/java/by/epam/task10/calendar/parser/FormatRespList.java
+++ task10/src/main/java/by/epam/task10/calendar/parser/FormatRespList.java
@@ -13,24 +13,30 @@ public class FormatRespList {
                     StringBuilder sb = new StringBuilder(e.getKey().toString() + "->\t");
                     for (FreeCelebrity freeCelebrity : e.getValue()) {
                         sb.append("\n\t\t");
-                        if (!freeCelebrity.getName().isEmpty()) {
-                            sb.append(freeCelebrity.getName()).append(": ");
-                        } else {
-                            sb.append("No name: ");
-                        }
-                        if (freeCelebrity.isCelebrity()) {
-                            sb.append(" Celebration;");
-                        }
-                        if (freeCelebrity.isFree()) {
-                            sb.append(" Free;");
-                        }
-                        if (!freeCelebrity.getDescription().isEmpty()) {
-                            sb.append("\t").append("Description: ").append(freeCelebrity.getDescription()).append(";");
-                        }
+                        enterName(sb, freeCelebrity);
+                        enterIfCondition(sb, freeCelebrity.isCelebrity(), " Celebration;");
+                        enterIfCondition(sb, freeCelebrity.isFree(), " Free;");
+                        enterIfCondition(sb.append("\t").append("Description: ").append(freeCelebrity.getDescription()),
+                                !freeCelebrity.getDescription().isEmpty(),
+                                ";");
                     }
                     return sb.append("\n");
                 })
                 .reduce(StringBuilder::append)
                 .orElse(new StringBuilder()).toString();
+    }
+
+    public void enterIfCondition(StringBuilder sb, boolean condition, String s) {
+        if (condition) {
+            sb.append(s);
+        }
+    }
+
+    public void enterName(StringBuilder sb, FreeCelebrity freeCelebrity) {
+        if (!freeCelebrity.getName().isEmpty()) {
+            sb.append(freeCelebrity.getName()).append(": ");
+        } else {
+            sb.append("No name: ");
+        }
     }
 }
