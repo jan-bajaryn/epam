0203547fafc7bd6183b02e3af49801c9a406ecb2
Jan <jabajaryn@gmail.com>
2020-02-01 03:57:02 +0300

    loggin fixed

--- task11_Cafe/src/main/java/by/epam/cafe/controller/Runner.java
+++ task11_Cafe/src/main/java/by/epam/cafe/controller/Runner.java
@@ -1,7 +1,14 @@
 package by.epam.cafe.controller;
 
+
+import org.apache.log4j.Logger;
+
 public class Runner {
+    public static Logger log = Logger.getLogger(Runner.class);
+
     public static void main(String[] args) {
+        log.info("adbcd");
         System.out.println("Hello");
+
     }
 }
