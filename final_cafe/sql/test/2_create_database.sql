233f37352a1a249a55d8e6dcca9df5de10f403e8
Jan <jabajaryn@gmail.com>
2020-04-01 23:58:16 +0300

    version with normal user

--- final_cafe/sql/test/2_create_database.sql
+++ final_cafe/sql/test/2_create_database.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS pizzeria_test DEFAULT CHARACTER SET utf8;
 
 DROP USER IF EXISTS 'pizza_worker_test'@'localhost';
 CREATE USER IF NOT EXISTS 'pizza_worker_test'@'localhost' IDENTIFIED BY 'abcde';
-CREATE USER IF NOT EXISTS 'pizza_worker_test'@'localhost' IDENTIFIED BY 'abcde';
 
 # GRANT ALL ON *.* TO 'pizza_worker_test'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'pizza_worker_test'@'localhost';
+# GRANT ALL PRIVILEGES ON pizzeria_test.* TO 'pizza_worker_test'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON pizzeria_test.* TO 'pizza_worker_test'@'localhost';
 
 FLUSH PRIVILEGES;
