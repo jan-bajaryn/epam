1ce8335e0a8f27a1a558145e8790f6ce7dabba3c
Jan <jabajaryn@gmail.com>
2020-04-01 23:49:33 +0300

    stable version of test

--- final_cafe/sql/test/2_create_database.sql
+++ final_cafe/sql/test/2_create_database.sql
@@ -1,5 +1,10 @@
 CREATE DATABASE IF NOT EXISTS pizzeria_test DEFAULT CHARACTER SET utf8;
 
-CREATE USER IF NOT EXISTS 'pizza_worker'@'localhost' IDENTIFIED BY 'abcde';
+DROP USER IF EXISTS 'pizza_worker_test'@'localhost';
+CREATE USER IF NOT EXISTS 'pizza_worker_test'@'localhost' IDENTIFIED BY 'abcde';
+CREATE USER IF NOT EXISTS 'pizza_worker_test'@'localhost' IDENTIFIED BY 'abcde';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON pizzeria_test.* TO 'pizza_test_worker'@'localhost';
+# GRANT ALL ON *.* TO 'pizza_worker_test'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'pizza_worker_test'@'localhost';
+
+FLUSH PRIVILEGES;
--- final_cafe/src/main/java/by/epam/cafe/dao/pool/ConnectionPool.java
+++ final_cafe/src/main/java/by/epam/cafe/dao/pool/ConnectionPool.java
@@ -3,6 +3,8 @@ package by.epam.cafe.dao.pool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -26,13 +28,12 @@ public class ConnectionPool {
     }
 
     public static final int MAX_COUNT = 100;
-    private static final String URL = "jdbc:mysql://localhost:3306/pizzeria";
     private static Properties properties = new Properties();
 
     private List<Connection> passive = new ArrayList<>();
     private List<Connection> active = new ArrayList<>();
 
-    private static final String RESOURCE = "property/database.properties";
+    private static final String RESOURCE = "/property/database.properties";
 
     private ReentrantLock locker = new ReentrantLock();
     private Condition condition = locker.newCondition();
@@ -49,22 +50,27 @@ public class ConnectionPool {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            InputStream is = ConnectionPool.class.getResourceAsStream(RESOURCE);
+            properties.load(is);
+        } catch (ClassNotFoundException | IOException e) {
+            log.fatal("Problems in initialization ConnectionPool", e);
+            throw new ExceptionInInitializerError(e);
         }
-        properties.put("user", "root");
-        properties.put("password", "pass");
-        properties.put("autoReconnect", "true");
-        properties.put("characterEncoding", "UTF-8");
-        properties.put("useUnicode", "true");
-        properties.put("serverTimezone", "UTC");
-        properties.put("useSSL", "false");
+//        properties.put("user", "root");
+//        properties.put("password", "pass");
+//        properties.put("autoReconnect", "true");
+//        properties.put("characterEncoding", "UTF-8");
+//        properties.put("useUnicode", "true");
+//        properties.put("serverTimezone", "UTC");
+//        properties.put("useSSL", "false");
 
     }
 
     public Connection takeConnection() {
         try {
-            return DriverManager.getConnection(URL, properties);
+            String url = properties.getProperty("url");
+            log.info("url = {}", url);
+            return DriverManager.getConnection(url, properties);
         } catch (SQLException e) {
             log.error("Exception in takeConnection: ", e);
             return null;
@@ -113,7 +119,7 @@ public class ConnectionPool {
             if (passive.size() + active.size() >= MAX_COUNT) {
                 return null;
             }
-            return DriverManager.getConnection(URL, properties);
+            return DriverManager.getConnection(properties.getProperty("url"), properties);
         } finally {
             condition.notifyAll();
             locker.unlock();
