628cfeb9eae5c36e7aca0fcd375f23fad354e250
Jan <jabajaryn@gmail.com>
2020-05-19 01:58:23 +0300

    basket validator added

--- final_cafe/src/test/java/by/epam/cafe/service/validator/BasketValidatorTest.java
+++ final_cafe/src/test/java/by/epam/cafe/service/validator/BasketValidatorTest.java
@@ -1,7 +1,12 @@
 package by.epam.cafe.service.validator;
 
+import by.epam.cafe.entity.db.impl.Product;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static by.epam.cafe.service.parser.helper.impl.ValidateAndPutter.POSTFIX;
 import static org.testng.Assert.*;
 
 public class BasketValidatorTest {
@@ -10,5 +15,56 @@ public class BasketValidatorTest {
 
     @Test
     public void testIsValid() {
+        Map<Product, Integer> basket = new HashMap<>();
+        Map<String, String> redirect = new HashMap<>();
+        String label = "a";
+        boolean valid = basketValidator.isValid(basket, redirect, label);
+        assertFalse(valid);
+    }
+
+    @Test
+    public void testIsValidNullInput() {
+        Map<Product, Integer> basket = null;
+        Map<String, String> redirect = new HashMap<>();
+        String label = "a";
+        boolean valid = basketValidator.isValid(basket, redirect, label);
+        assertFalse(valid);
+    }
+
+    @Test
+    public void testIsValidNullInputRedirect() {
+        Map<Product, Integer> basket = new HashMap<>();
+        Map<String, String> redirect = null;
+        String label = "a";
+        assertThrows(NullPointerException.class, () -> basketValidator.isValid(basket, redirect, label));
+    }
+
+    @Test
+    public void testIsValidCheckCallback() {
+        Map<Product, Integer> basket = new HashMap<>();
+        Map<String, String> redirect = new HashMap<>();
+        String label = "a";
+        basketValidator.isValid(basket, redirect, label);
+        assertTrue(redirect.containsKey("a" + POSTFIX));
+    }
+
+    @Test
+    public void testIsValidPositive() {
+        Map<Product, Integer> basket = new HashMap<>();
+        basket.put(Product.newBuilder().id(1).build(), 1);
+        Map<String, String> redirect = new HashMap<>();
+        String label = "a";
+        boolean valid = basketValidator.isValid(basket, redirect, label);
+        assertTrue(valid);
+    }
+
+    @Test
+    public void testIsValidNegative() {
+        Map<Product, Integer> basket = new HashMap<>();
+        basket.put(Product.newBuilder().id(1).build(), -1);
+        Map<String, String> redirect = new HashMap<>();
+        String label = "a";
+        boolean valid = basketValidator.isValid(basket, redirect, label);
+        assertFalse(valid);
     }
 }
